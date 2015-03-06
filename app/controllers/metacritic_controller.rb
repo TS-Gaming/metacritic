80688c46fb3788a9e6411fb563f1f2f2009ab662
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-03-06 13:34:53 -0500

    Metacritc env var token added

--- app/controllers/metacritic_controller.rb
+++ app/controllers/metacritic_controller.rb
@@ -1,7 +1,33 @@
 class MetacriticController < ApplicationController
   def show
+    @response = HTTParty.get("https://byroredux-metacritic.p.mashape.com/find/game"),
+    headers:{
+      "X-Mashape-Key" => "#{ENV['METACRITIC_TOKEN']}",
+      "Content-Type" => "application/x-www-form-urlencoded",
+      "Accept" => "application/json"
+    },
+    parameters:{
+      "platform" => 1,
+      "retry" => 4,
+      "title" => "#{params[:id]}"
+      }
+    if @response["_total"] == 0
+      redirect_to root_path
+    end    
   end
 
   def index
+    # These code snippets use an open-source library.
+    @response = HTTParty.get("https://byroredux-metacritic.p.mashape.com/find/game"),
+    headers:{
+      "X-Mashape-Key" => "#{ENV['METACRITIC_TOKEN']}",
+      "Content-Type" => "application/x-www-form-urlencoded",
+      "Accept" => "application/json"
+    },
+    parameters:{
+      "platform" => 1,
+      "retry" => 4,
+      "title" => "#{params[:id]}"
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'metacritic/show'
-
-  get 'metacritic/index'
+  resources :metacritic
+  root 'metacritc#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
